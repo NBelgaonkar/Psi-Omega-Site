cad105a3aa66ed40d28c4de857b905bfeddebb9c
NBelgaonkar <110398341+NBelgaonkar@users.noreply.github.com>
2023-10-10 17:30:29 -0400

    Made An ABout Us Page Highlighting some details and am looking to expand it.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 const AboutUs = () => {
-  return (
-    <div>
-      <h2>About</h2>
-      <p>This is the about us page. Add your content here.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>About Us</h2>
+            <h3>Delta Kappa Epsilon - Psi Omega Chapter</h3>
+            <p>Located at the heart of Rensselaer Polytechnic Institute, 901 Peoples Avenue, the Psi Omega Chapter of Delta Kappa Epsilon (DKE) stands as a beacon of brotherhood and excellence. Since its inception, our fraternity has been devoted to upholding our cherished motto - “Friends from the heart forever.” As a post-pandemic fraternity, we've reemerged stronger, more enthusiastic, and steadfast in our core values.</p>
+
+            <h4>At A Glance</h4>
+            <ul>
+                <li>Chapter Size: 24 dedicated members</li>
+                <li>GPA Average: A proud 3.34</li>
+                <li>Philanthropy: We raised over $1000 this year for charitable causes</li>
+                <li>Community Service: Each member commits to 8 hours of community service</li>
+                <li>Diverse Majors: Our brothers represent the School of Business, Engineering, Humanities, and Science.</li>
+            </ul>
+
+
+        </div>
+    );
 };
 
 export default AboutUs;
+
