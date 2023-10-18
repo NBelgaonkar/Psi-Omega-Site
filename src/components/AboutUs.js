9fd5eb65be14edfb7c53e18973f5191f092cc1ff
NBelgaonkar <110398341+NBelgaonkar@users.noreply.github.com>
2023-10-18 18:15:53 -0400

    Expanded the About US Page- Added a scrolling an Eboard Position list that shows each eboard position and allows them to add pictures of there eboard.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const eboardMembers = [
+    { position: 'President', photo: 'path_to_photo' },
+    { position: 'Vice President', photo: 'path_to_photo' },
+    { position: 'Social Chair', photo: 'path_to_photo' },
+    { position: 'Recruitment Chair', photo: 'path_to_photo' },
+    { position: 'Health & Safety Chair', photo: 'path_to_photo' },
+    { position: 'Treasurer', photo: 'path_to_photo' },
+    { position: 'Philanthropy Chair', photo: 'path_to_photo' },
+    { position: 'Alumni Relations Chair', photo: 'path_to_photo' },
+    { position: 'Marketing Chair', photo: 'path_to_photo' }
+];
 
 const AboutUs = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveIndex((prevIndex) =>
+                (prevIndex + 1) % eboardMembers.length
+            );
+        }, 3000);  // change slides every 3 seconds
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div>
             <h2>About Us</h2>
@@ -18,17 +42,39 @@ const AboutUs = () => {
 
             <h4>Our Mission</h4>
             <p><strong>Academic Excellence:</strong> We prioritize our studies, with a dedicated Academic Chair supporting those who strive for academic greatness.</p>
-
             <p><strong>Moral & Cultural Development:</strong> Our leadership structures and external community involvement ensures our brothers grow holistically, absorbing diverse perspectives and fostering leadership skills.</p>
-
             <p><strong>Community Engagement:</strong> We actively serve our community, from our partnership with Oakwood Community Center to campus-wide events, always upholding our core values.</p>
-
             <p><strong>Human Connection:</strong> Brotherhood is the cornerstone of our fraternity. It's not just about spending time together but supporting one another academically, professionally, and personally.</p>
-
             <p><strong>Behind The Scenes:</strong> The Psi Omega Chapter of DKE is not just a fraternity; it's a legacy. Upheld by our Objects, we believe in intellectual excellence, honorable friendship, tolerance, and the unity of kindred spirits.</p>
+
+            <h4>Executive Board</h4>
+            <div className="carousel">
+                <img src={eboardMembers[activeIndex].photo} alt={`${eboardMembers[activeIndex].position}'s Photo`} />
+                <h5>{eboardMembers[activeIndex].position}</h5>
+            </div>
         </div>
     );
 };
 
 export default AboutUs;
 
+// Add this CSS to style the carousel
+/*
+.carousel {
+    width: 200px;  // or any desired width
+    height: 300px;  // or any desired height
+    position: relative;
+    overflow: hidden;
+}
+
+.carousel img {
+    width: 100%;
+    height: 80%;
+    object-fit: cover;
+}
+
+.carousel h5 {
+    text-align: center;
+    padding: 10px 0;
+}
+*/
