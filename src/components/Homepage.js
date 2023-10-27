e869930a790ec896331a343bea8c6b88c11b21dc
whhlct <39563523+whhlct@users.noreply.github.com>
2023-10-27 16:39:20 -0400

    Adding sections to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -16,6 +16,21 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const AnnouncementSection = styled.div`
+  margin-top: 30px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  width: 80%;
+  text-align: left;
+`;
+
+const EventSection = styled.div`
+  margin-top: 30px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  width: 80%;
+  text-align: left;
+`;
 
 class Homepage extends React.Component {
   
@@ -24,7 +39,25 @@ class Homepage extends React.Component {
     return (
       <Container>
         <h1>Welcome to DKE</h1>
-        
+        <p>
+          Discover the rich history, traditions, and activities of the Psi Omega Chapter of Delta Kappa Epsilon. Stay informed and connected with our community.
+        </p>
+
+        <AnnouncementSection>
+          <h2>Announcements</h2>
+          <p>
+            Stay tuned for the latest updates and important announcements from our chapter.
+            {/* You can dynamically fetch and display announcements here */}
+          </p>
+        </AnnouncementSection>
+
+        <EventSection>
+          <h2>Upcoming Events</h2>
+          <p>
+            Don't miss out on our upcoming events and gatherings. Mark your calendars!
+            {/* You can dynamically fetch and display upcoming events here */}
+          </p>
+        </EventSection>
       </Container>
     );
   }
