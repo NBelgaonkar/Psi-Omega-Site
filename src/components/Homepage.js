c7c1d5c6440d5a8d14f0800167abd33528bcd62e
whhlct <39563523+whhlct@users.noreply.github.com>
2023-10-27 16:42:48 -0400

    Updated sections of homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -16,16 +16,8 @@ const Container = styled.div`
   width: 100%;
 `;
 
-const AnnouncementSection = styled.div`
-  margin-top: 30px;
-  border: 1px solid #ccc;
-  padding: 20px;
-  width: 80%;
-  text-align: left;
-`;
-
-const EventSection = styled.div`
-  margin-top: 30px;
+const Section = styled.div`
+  margin-top: 20px;
   border: 1px solid #ccc;
   padding: 20px;
   width: 80%;
@@ -43,21 +35,38 @@ class Homepage extends React.Component {
           Discover the rich history, traditions, and activities of the Psi Omega Chapter of Delta Kappa Epsilon. Stay informed and connected with our community.
         </p>
 
-        <AnnouncementSection>
+        <Section>
           <h2>Announcements</h2>
           <p>
             Stay tuned for the latest updates and important announcements from our chapter.
             {/* You can dynamically fetch and display announcements here */}
           </p>
-        </AnnouncementSection>
+        </Section>
 
-        <EventSection>
+        <Section>
           <h2>Upcoming Events</h2>
           <p>
             Don't miss out on our upcoming events and gatherings. Mark your calendars!
             {/* You can dynamically fetch and display upcoming events here */}
           </p>
-        </EventSection>
+        </Section>
+
+        <Section>
+          <h2>Chapter Information</h2>
+          <p>
+            Learn about the Psi Omega Chapter of Delta Kappa Epsilon. Discover our location, the history of our chapter, and the notable awards and achievements we've garnered over the years.
+            {/* You can dynamically fetch and display detailed chapter information here */}
+          </p>
+        </Section>
+
+        <Section>
+          <h2>Fraternity History</h2>
+          <p>
+            Dive deep into the history and traditions of the Delta Kappa Epsilon fraternity. Understand our legacy both at the chapter level and within the national organization.
+            {/* You can dynamically fetch and display the fraternity's history here */}
+          </p>
+        </Section>
+
       </Container>
     );
   }
