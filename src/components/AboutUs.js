80215eb0d9ba8566670fb16ede5c2f583cf098a0
NBelgaonkar <110398341+NBelgaonkar@users.noreply.github.com>
2023-11-03 18:02:27 -0400

    The AboutUs page had several design changes that improved its structure. The info was reformatted so it looks better and had a map blurb added that works with google maps api. For more efficient code management, CSS styles are embedded directly into the React component. In addition, the eboard slider was optimized. Finally, changes have been made to the responsive design so that the devices can be viewed smoothly.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -17,64 +17,96 @@ const AboutUs = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setActiveIndex((prevIndex) =>
-                (prevIndex + 1) % eboardMembers.length
-            );
+            setActiveIndex((prevIndex) => (prevIndex + 1) % eboardMembers.length);
         }, 3000);  // change slides every 3 seconds
 
         return () => clearInterval(interval);
     }, []);
 
     return (
-        <div>
-            <h2>About Us</h2>
-            <h3>Delta Kappa Epsilon - Psi Omega Chapter</h3>
-            <p>Located at the heart of Rensselaer Polytechnic Institute, 901 Peoples Avenue, the Psi Omega Chapter of Delta Kappa Epsilon (DKE) stands as a beacon of brotherhood and excellence. Since its inception, our fraternity has been devoted to upholding our cherished motto - “Friends from the heart forever.” As a post-pandemic fraternity, we've reemerged stronger, more enthusiastic, and steadfast in our core values.</p>
+        <div className="about-us-container">
+            <div className="about-us-content">
+                <h2>About Us</h2>
+                <h3>Delta Kappa Epsilon - Psi Omega Chapter</h3>
+                <p>Located at the heart of Rensselaer Polytechnic Institute, 901 Peoples Avenue, the Psi Omega Chapter of Delta Kappa Epsilon (DKE) stands as a beacon of brotherhood and excellence.</p>
 
-            <h4>At A Glance</h4>
-            <ul>
-                <li>Chapter Size: 24 dedicated members</li>
-                <li>GPA Average: A proud 3.34</li>
-                <li>Philanthropy: We raised over $1000 this year for charitable causes</li>
-                <li>Community Service: Each member commits to at least 10 hours of community service</li>
-                <li>Diverse Majors: Our brothers represent the School of Business, Engineering, Humanities, and Science.</li>
-            </ul>
+                <h4>At A Glance</h4>
+                <ul>
+                    <li>Chapter Size: 24 dedicated members</li>
+                    <li>GPA Average: A proud 3.34</li>
+                    <li>Philanthropy: We raised over $1000 this year for charitable causes</li>
+                    <li>Community Service: Each member commits to at least 10 hours of community service</li>
+                    <li>Diverse Majors: Our brothers represent the School of Business, Engineering, Humanities, and Science.</li>
+                </ul>
 
-            <h4>Our Mission</h4>
-            <p><strong>Academic Excellence:</strong> We prioritize our studies, with a dedicated Academic Chair supporting those who strive for academic greatness.</p>
-            <p><strong>Moral & Cultural Development:</strong> Our leadership structures and external community involvement ensures our brothers grow holistically, absorbing diverse perspectives and fostering leadership skills.</p>
-            <p><strong>Community Engagement:</strong> We actively serve our community, from our partnership with Oakwood Community Center to campus-wide events, always upholding our core values.</p>
-            <p><strong>Human Connection:</strong> Brotherhood is the cornerstone of our fraternity. It's not just about spending time together but supporting one another academically, professionally, and personally.</p>
-            <p><strong>Behind The Scenes:</strong> The Psi Omega Chapter of DKE is not just a fraternity; it's a legacy. Upheld by our Objects, we believe in intellectual excellence, honorable friendship, tolerance, and the unity of kindred spirits.</p>
-
-            <h4>Executive Board</h4>
-            <div className="carousel">
-                <img src={eboardMembers[activeIndex].photo} alt={`${eboardMembers[activeIndex].position}'s Photo`} />
-                <h5>{eboardMembers[activeIndex].position}</h5>
+                <h4>Our Mission</h4>
+                <p><strong>Academic Excellence:</strong> We prioritize our studies, with a dedicated Academic Chair supporting those who strive for academic greatness.</p>
+                <p><strong>Moral & Cultural Development:</strong> Our leadership structures and external community involvement ensure our brothers grow holistically, absorbing diverse perspectives and fostering leadership skills.</p>
+                <p><strong>Community Engagement:</strong> We actively serve our community, from our partnership with Oakwood Community Center to campus-wide events, always upholding our core values.</p>
+                <p><strong>Human Connection:</strong> Brotherhood is the cornerstone of our fraternity. It's not just about spending time together but supporting one another academically, professionally, and personally.</p>
+                <p><strong>Behind The Scenes:</strong> The Psi Omega Chapter of DKE is not just a fraternity; it's a legacy. Upheld by our Objects, we believe in intellectual excellence, honorable friendship, tolerance, and the unity of kindred spirits.</p>
             </div>
+
+            <div className="map-and-eboard">
+                <div className="google-map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2920.22522574596!2d-73.67892508451474!3d42.72940517916625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de0b4ad1f9da57%3A0x60c67c91b79a2038!2s901%20Peoples%20Ave%2C%20Troy%2C%20NY%2012180%2C%20USA!5e0!3m2!1sen!2sus!4v1634556527181!5m2!1sen!2sus"
+                        width="400"
+                        height="300"
+                        allowFullScreen=""
+                        loading="lazy"
+                        title="Location of Delta Kappa Epsilon - Psi Omega Chapter"
+                    ></iframe>
+                </div>
+                <div className="carousel">
+                    <img src={eboardMembers[activeIndex].photo} alt={`${eboardMembers[activeIndex].position}'s Photo`} />
+                    <h5>{eboardMembers[activeIndex].position}</h5>
+                </div>
+            </div>
+
+            <style jsx>{`
+                .about-us-container {
+                    display: flex;
+                    justify-content: space-between;
+                    width: 100%;
+                }
+
+                .about-us-content {
+                    flex: 1;
+                    margin-right: 20px;
+                }
+
+                .map-and-eboard {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    width: 400px;
+                }
+
+                .google-map {
+                    margin-bottom: 20px;
+                }
+
+                .carousel {
+                    width: 200px;
+                    height: 300px;
+                    position: relative;
+                    overflow: hidden;
+                }
+
+                .carousel img {
+                    width: 100%;
+                    height: 80%;
+                    object-fit: cover;
+                }
+
+                .carousel h5 {
+                    text-align: center;
+                    padding: 10px 0;
+                }
+            `}</style>
         </div>
     );
 };
 
 export default AboutUs;
-
-// Add this CSS to style the carousel
-/*
-.carousel {
-    width: 200px;  // or any desired width
-    height: 300px;  // or any desired height
-    position: relative;
-    overflow: hidden;
-}
-
-.carousel img {
-    width: 100%;
-    height: 80%;
-    object-fit: cover;
-}
-
-.carousel h5 {
-    text-align: center;
-    padding: 10px 0;
-}
-*/
