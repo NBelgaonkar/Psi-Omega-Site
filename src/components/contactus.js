2c6365421cbf81d65e0800250b3d917431112845
JakeMandel03 <mandej@rpi.edu>
2023-12-01 15:35:58 -0500

    Create contactus.js

---
+++ src/components/contactus.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import background from '../Images/pic.png';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${background});
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center top;
+  color: black;
+  flex-grow: 1;
+  min-height: 100vh;
+  padding-top: 60px;
+  padding: 50px;
+  margin-top: 60px;
+`;
+
+const ContactSection = styled.div`
+  text-align: left;
+  max-width: 460px;
+  margin-top: 150px;
+  margin-bottom: 300px;
+  color: black;
+  p {
+    font-size: 24px; 
+  }
+  h1 {
+    font-size: 36px; 
+    margin-bottom: 30px;
+  }
+`;
+
+const ContactInfo = styled.p`
+  margin-bottom: 20px;
+  font-size: 20px;
+  line-height: 1.6;
+`;
+
+class ContactPage extends React.Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <Container>
+          <ContactSection>
+            <h1>Contact Us</h1>
+            <ContactInfo>
+              <strong>DKE Fraternity House Address:</strong><br />
+              901 Peoples Ave, Troy, NY
+            </ContactInfo>
+            <ContactInfo>
+              <strong>RPI Address:</strong><br />
+              110 8th St, Troy, NY
+            </ContactInfo>
+            <ContactInfo>
+              <strong>Email:</strong><br />
+              <a href="mailto:contactus@dkerpi.com">contactus@dkerpi.com</a>
+            </ContactInfo>
+          </ContactSection>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default ContactPage;
