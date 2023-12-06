041e8e0129e46693799cff17f2d11485e555a942
whhlct <39563523+whhlct@users.noreply.github.com>
2023-12-05 19:18:00 -0500

    Made further fixes to rush page

--- src/components/rushdke.js
+++ src/components/rushdke.js
@@ -2,28 +2,54 @@ import React from 'react';
 import styled from 'styled-components';
 import background from '../Images/pic.png';
 
-// Additional imports for new components
-import Header from './Header'; // Sample Header component
-import Footer from './Footer'; // Sample Footer component
-import Calendar from './Calendar'; // Sample Calendar component
-import FAQ from './FAQ'; // Sample FAQ component
-import ContactForm from './ContactForm'; // Sample Contact Form component
-import Gallery from './Gallery'; // Sample Gallery component
-
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Open Sans', sans-serif;
   }
 `;
 
-// Existing styled components...
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${background});
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center top;
+  color: black;
+  flex-grow: 1;
+  min-height: 100vh;
+  padding-top: 60px;
+  padding: 50px;
+  margin-top: 60px;
+`;
+
+const RushSection = styled.div`
+  text-align: left;
+  max-width: 460px;
+  margin-top: 150px;
+  margin-bottom: 300px;
+  color: black;
+  p {
+    font-size: 24px;
+  }
+  h1 {
+    font-size: 36px;
+    margin-bottom: 30px;
+  }
+`;
+
+const RushInfo = styled.p`
+  margin-bottom: 20px;
+  font-size: 20px;
+  line-height: 1.6;
+`;
 
 class RushDKEPage extends React.Component {
   render() {
     return (
       <>
         <GlobalStyle />
-        <Header />
         <Container>
           <RushSection>
             <h1>Rush DKE</h1>
@@ -31,21 +57,16 @@ class RushDKEPage extends React.Component {
               <strong>What does it mean to rush DKE:</strong> Rushing DKE means you are interested in becoming a member of our brotherhood. To do this, attend events we host at the beginning of the semester to familiarize yourself with the brothers and what we do.
             </RushInfo>
             <RushInfo>
-              <strong>Questions?</strong> If you have any questions, feel free to contact our recruitment chair through the contact form below.
+              <strong>Questions?</strong> Contact our recruitment chair.
             </RushInfo>
             <RushInfo>
-              <strong>DKE Rush Calendar:</strong> Check out our interactive calendar for all the important dates for rushing DKE.
+              <strong>DKE Rush Calendar:</strong> Available at the start of the fall and spring semesters.
             </RushInfo>
-            <Calendar />
             <RushInfo>
-              <strong>DKE Events Calendar:</strong> Refer to the main calendar on the homepage for details about all our events.
+              <strong>DKE Events Calendar:</strong> Refer to main calendar on homepage for details
             </RushInfo>
-            <FAQ />
-            <Gallery />
-            <ContactForm />
           </RushSection>
         </Container>
-        <Footer />
       </>
     );
   }
