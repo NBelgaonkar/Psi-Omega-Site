fb840e879c72f56b4d10da7a6d290c871097bd61
whhlct <39563523+whhlct@users.noreply.github.com>
2023-12-05 19:33:24 -0500

    fixed website header

--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import background from '../Images/pic.png';
+import createGlobalStyle from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
