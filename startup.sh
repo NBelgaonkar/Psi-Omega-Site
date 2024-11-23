7ba70a1c69f1cff6855a4ad13277b56af4e3ed75
Alexander <thisisouranswer@gmail.com>
2024-11-23 01:56:08 -0500

    simplyfing startup script

--- startup.sh
+++ startup.sh
@@ -4,32 +4,6 @@
 echo "Building and starting services..."
 docker-compose up --build -d
 
-# Check for SSL certificates
-if [ ! -f "./certbot/conf/live/dekesrpi.org/fullchain.pem" ]; then
-  echo "Certificates missing! Running Certbot to generate certificates..."
-  
-  # Stop Nginx
-  echo "Stopping Nginx..."
-  docker-compose stop nginx
-
-  # Generate certificates
-  echo "Running Certbot..."
-  docker run --rm \
-    -v $(pwd)/certbot/conf:/etc/letsencrypt \
-    -v $(pwd)/certbot/www:/var/www/certbot \
-    certbot/certbot certonly --standalone \
-    --preferred-challenges http \
-    --email it.dkepsiomega@gmail.com \
-    --agree-tos \
-    -d dekesrpi.org -d www.dekesrpi.org
-
-  # Restart Nginx
-  echo "Restarting Nginx..."
-  docker-compose up -d nginx
-else
-  echo "Certificates already exist. Skipping Certbot."
-fi
-
 # Check container status
 echo "Checking container status..."
 docker-compose ps
