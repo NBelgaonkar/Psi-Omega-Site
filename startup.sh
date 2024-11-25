efef9bc2eb00ca2e6a80fd75f9ca3cf0eeba11cf
Alexander <thisisouranswer@gmail.com>
2024-11-25 16:20:36 -0500

    following medium web guide on https

--- startup.sh
+++ startup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Stop and remove any existing containers
+echo "Cleaning up existing containers..."
+docker-compose down
+
 # Build and start services
 echo "Building and starting services..."
 docker-compose up --build -d
 
-# Check container status
-echo "Checking container status..."
-docker-compose ps
+# Generate SSL certificate
+echo "Generating SSL certificate..."
+docker run --rm -v /etc/letsencrypt:/etc/letsencrypt \
+  -v /var/lib/letsencrypt:/var/lib/letsencrypt \
+  certbot/certbot certonly --webroot \
+  --webroot-path=/var/www/certbot \
+  --agree-tos --no-eff-email --email it.dkepsiomega@gmail.com \
+  -d dekesrpi.org -d www.dekesrpi.org
+
+# Reload nginx with the new certificate
+echo "Reloading nginx..."
+docker-compose exec web nginx -s reload
