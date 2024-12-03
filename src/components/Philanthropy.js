bc087aeb027f2b588d3108e04abd5a0fbefeef6e
crispyn4cho <cristn@rpi.edu>
2024-12-03 14:54:20 -0500

    fixing philanthropy page
    philanthropy page was not showing any content so I changed that, uploaded oakwood and spec olympics logos

--- src/components/Philanthropy.js
+++ src/components/Philanthropy.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
-//import oakwoodLogo from '../Images/oakwood_community_center.png'; // Example logo path
-//import specialOlympicsLogo from '../Images/special_olympics_logo.png'; // Example logo path
-
+import oakwoodLogo from '../Images/oakwood.png';
+import specialOlympicsLogo from '../Images/specialOlympics.png';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Literata:wght@400;700&display=swap');
@@ -32,22 +31,29 @@ const Title = styled.h1`
 const PhilanthropyRow = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: flex-start;
   flex-wrap: wrap;
   max-width: 1000px;
+  gap: 20px;
 `;
 
 const PhilanthropyItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
   width: 45%;
-  margin-bottom: 40px;
-  text-align: left;
 `;
 
 const Logo = styled.img`
-  max-width: 200px;
+  max-width: 150px;
+  height: auto;
   margin-bottom: 20px;
+  min-height: 100px; /* Ensures consistent space for logos */
+  padding: 10px; /* Adds extra space around the logo */
 `;
 
-const PhilanthropyDescription = styled.p`
+const PhilanthropyDescription = styled.div`
   font-size: 18px;
   color: black;
   line-height: 1.6;
@@ -64,37 +70,46 @@ const PhilanthropyLink = styled.a`
 
 const Philanthropy = () => {
   return (
-    <GlobalStyle>
+    <>
+      <GlobalStyle />
       <PhilanthropyContainer>
         <Title>Philanthropy</Title>
 
         <PhilanthropyRow>
           {/* Oakwood Community Center Section */}
           <PhilanthropyItem>
+            <Logo src={oakwoodLogo} alt="Oakwood Community Center Logo" />
             <PhilanthropyDescription>
               Every weekend, DKE Brothers volunteer to assist the Oakwood Community Center’s Weekly Food Pantry.
               Learn more about their mission here:{' '}
-              {/* <PhilanthropyLink href="https://www.oakwoodcommunitycenter.org/food-pantry" target="_blank"> */}
-                https://www.oakwoodcommunitycenter.org/food-pantry
-              {/* </PhilanthropyLink>  */}
+              <PhilanthropyLink
+                href="https://www.oakwoodcommunitycenter.org/food-pantry"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Oakwood Community Center Food Pantry
+              </PhilanthropyLink>
             </PhilanthropyDescription>
-            {/* <Logo src={oakwoodLogo} alt="Oakwood Community Center Logo" /> */}
           </PhilanthropyItem>
 
           {/* Special Olympics Section */}
           <PhilanthropyItem>
+            <Logo src={specialOlympicsLogo} alt="Special Olympics Logo" />
             <PhilanthropyDescription>
               Once per semester, DKE runs a pancake delivery event we call DKE’s Diner to support the Special Olympics.
               Learn more about their organization here:{' '}
-              {/* <PhilanthropyLink href="https://www.specialolympics.org/" target="_blank"> */}
-                {/* https://www.specialolympics.org/ */}
-              {/* </PhilanthropyLink> */}
+              <PhilanthropyLink
+                href="https://www.specialolympics.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Special Olympics
+              </PhilanthropyLink>
             </PhilanthropyDescription>
-            {/* <Logo src={specialOlympicsLogo} alt="Special Olympics Logo" /> */}
           </PhilanthropyItem>
         </PhilanthropyRow>
       </PhilanthropyContainer>
-    </GlobalStyle>
+    </>
   );
 };
 
