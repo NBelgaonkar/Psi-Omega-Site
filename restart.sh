aa0820b69bbf0c047eb66d14bf3fe4c2dd5a3951
Alexander <thisisouranswer@gmail.com>
2024-12-20 20:40:37 -0500

    making sure databases dont dissapear on restart

--- restart.sh
+++ restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# kill containers
-docker kill $(docker ps -a -q)
+# Kill all running containers
+docker kill $(docker ps -q)
 
-# remove containers
-docker rm $(docker ps -a -q)
+# Remove all containers except PostgreSQL
+docker ps -a --format "{{.ID}} {{.Names}}" | grep -v "psi-omega-site_psql_1" | awk '{print $1}' | xargs -r docker rm
 
-# remove storage 
-docker system prune -a --volumes -f
+# Remove all unused images, containers, and volumes except PostgreSQL volumes
+docker system prune -a --volumes -f --filter "label!=postgres"
 
 # call startup.sh
 sudo sh ./startup.sh
