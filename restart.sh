16d34c176a555b6c545157b362b7702f3ebd1126
Alexander <thisisouranswer@gmail.com>
2024-12-20 23:57:05 -0500

    reverting restart script because it didnt work

--- restart.sh
+++ restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Kill all running containers
-docker kill $(docker ps -q)
+# kill containers
+docker kill $(docker ps -a -q)
 
-# Remove all containers except PostgreSQL
-docker ps -a --format "{{.ID}} {{.Names}}" | grep -v "psi-omega-site_psql_1" | awk '{print $1}' | xargs -r docker rm
+# remove containers
+docker rm $(docker ps -a -q)
 
-# Remove all unused images, containers, and volumes except PostgreSQL volumes
-docker system prune -a --volumes -f --filter "label!=postgres"
+# remove storage 
+docker system prune -a --volumes -f
 
 # call startup.sh
 sudo sh ./startup.sh
